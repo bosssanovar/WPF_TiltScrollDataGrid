a269e2661a3bc49f3861ff64f8bdaae52cd3d5e8
kei ito <ito.kei@jvckenwood.com>
2023-12-01 14:33:26 +0900

    first commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Detail> details = new List<Detail>()
+            {
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa", true, false, "DDDDEEEEEEEEEEEEEEEDDDD", "EEEEEEEEEEEEe", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", true, false, "DDDDDEEEEEEEEEEEEEDDDDDDDDDDD", "EEEEEEEEEEEEEEEEEEEEEEE", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+                new("AAAAAAAAAAAAAAAa", true, false, "DDDDDDDD", "E", 10, 2.2, HHHHH.bb),
+            };
+
+            grid.ItemsSource = details;
+        }
+    }
+
+    public record Detail(string A, bool B, bool C, string D, string E, int F, double G, HHHHH H)
+    {
+
+    }
+
+    public enum HHHHH
+    {
+        aaa, bb, cccc, dddddd,
+    }
+}
